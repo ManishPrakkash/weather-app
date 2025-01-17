88185a4a5feab21c62d188943c3b8550735d94fb
ManishPrakkash <manishprakkash@gmail.com>
2025-01-17 05:17:33 +0000

    png imported into app.jsx

--- weatherApp/src/App.jsx
+++ weatherApp/src/App.jsx
@@ -1,13 +1,28 @@
 import { useState } from 'react'
 import './App.css'
 
+import searchIcon from './assets/search.png'
+import clearIcon from './assets/clear.png'
+import cloudIcon from './assets/cloud.png'
+import drizzleIcon from './assets/drizzle.png'
+import rainIcon from './assets/rain.png'
+import windIcon from './assets/wind.png'
+import snowIcon from './assets/snow.png'
+import humidityIcon from './assets/humidity.png'
+
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <div>
-      
+    <div className="container">
+      <div className="input-container">
+        <input type="text" className="cityInput" placeholder="Search City"></input>
+        <div>
+          <img src={searchIcon} alt="searchicon"></img>
+        </div>
+      </div>
     </div>
     </>
   )
