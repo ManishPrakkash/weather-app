cd266da11faa8896c21459cfe69b8388d3d7d24d
ManishPrakkash <manishprakkash@gmail.com>
2025-01-17 03:49:40 +0000

    changes in imports

--- weatherApp/src/App.jsx
+++ weatherApp/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,10 +6,9 @@ function App() {
 
   return (
     <>
-      <div>
-        
-      </div>
+    <div>
       
+    </div>
     </>
   )
 }
